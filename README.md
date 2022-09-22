217b13652cb55ffff7ac91a1d0214379df4520af
qls8044 <gpqls9628@gmail.com>
2022-09-22 14:05:20 +0900

    홈플 수정 전

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # recipes_project
+
+## 프로젝트 목표 : 마트 영수증 이미지에서 텍스트를 추출해 구매한 상품과 연관 있는 레시피 추천하는 시스템 생성
+
+## 주요 사용 기능
+  - 데이터 수집 : selenium.webdriver, BeautifulSoup, request
+  - 전처리 : 정규식 활용(특수기호, 숫자 등 제거), konlpy의 Okt(데이터 토큰화, 불용어 제거)
+  - OCR : easyOcr(영수증 이미지를 텍스트로 변환)
+  - 학습 : Word2Vec(토큰화된 데이터를 학습 시켜 입력값과 유사한 단어 추출)
+  - 시각화 : Qt Designer
+  
+# 주요 코드 및 결과
+
+## 데이터 수집
+### 만개의 레시피 웹페이지에서 각 레시피 url 데이터 크롤링
+- 파일 : crawling_link.ipynb
+#### 주요 코드
+![링크 크롤링 코드](https://user-images.githubusercontent.com/108312150/191641441-f312862b-0b73-41ef-99c1-4b30f34b59cf.png)
+#### 결과
+![링크 크롤링 결과](https://user-images.githubusercontent.com/108312150/191641378-b27e78f4-3e3e-41c6-ac7f-76b984c372ad.png)
+
+### 레시피 이름과 재료 데이터 추출
+- 파일 : crawling_all.ipynb
+#### 주요 코드
+![레시피 이름,재료 크롤링 코드1](https://user-images.githubusercontent.com/108312150/191641626-a4cb8886-8428-4677-bca1-913457cb4760.png)
+#### 결과
+![레시피 이름,재료 크롤링 결과](https://user-images.githubusercontent.com/108312150/191641639-a7213631-8b97-4e3a-8f20-416880ceabb1.png)
+
+## 전처리
+### 학습을 위해 레시피 재료 전처리
+- 파일 : lotte_final.ipynb
+  - Okt를 이용해 재료 데이터 토큰화
+  - 정규식을 이용해 재료 데이터에서 한글을 제외한 숫자, 영어, 특수문자 등을 제거
+  - 불용어를 stopword로 직접 지정해 재료와 관련없는 문자 삭제
+#### 주요 코드
+![전처리 코드1](https://user-images.githubusercontent.com/108312150/191641925-1dbb5772-2474-4e83-998a-a42457639801.png)
+#### 결과
+![전처리 결과1](https://user-images.githubusercontent.com/108312150/191641995-27c0edd3-26ca-40b3-803d-82315c45b507.png)
+
+## OCR
+### easyOcr을 이용해 영수증 이미지에서 텍스트 추출
+- 파일 : lotte_final.ipynb
+#### 주요 코드
+![전처리 영수증,텍스트 변환 코드](https://user-images.githubusercontent.com/108312150/191643407-e99e56cc-d253-4899-93fb-0ea19843213e.png)
+#### 결과
+![영수증,텍스트 변환 결과1](https://user-images.githubusercontent.com/108312150/191642231-1deeff79-46a5-40a7-a177-c1e4ea035f90.png)
+
+## 학습
+### Word2Vec(토큰화된 재료 데이터를 학습시켜 입력값과 가장 유사한 단어를 추출하도록 학습)
+- 파일 : lotte_final.ipynb
+#### 주요 코드
+![학습 코드1](https://user-images.githubusercontent.com/108312150/191642329-4115343c-9398-4643-82fb-a7a700fc741a.png)
+'우유'를 입력했을 때 '우유'와 가장 유사한 단어 10개 추출
+#### 결과
+![학습 코드2](https://user-images.githubusercontent.com/108312150/191642396-561e447b-391d-4c0a-b356-71179c003f6b.png)
+영수증에서 추출한 텍스트를 유사한 단어로 치환해서 '서울우유'를 '우유'로 자동 변환하는 기능을 구현하고 싶었지만, 데이터가 부족해 학습히 원활히 이루어지지 않아 구매한 상품명은 임의로 지정함
+  
+## 시각화
+### Qt Designer를 활용해 시스템 시각화
+- 파일 : pyqt_recipes.py, recipes.ui
+  - 구매한 상품명을 콤보 박스에 담음
+  - 콤보 박스에서 해당 상품을 클릭하면 (해당 상품 + 상품과 가장 유사한 재료)를 자동으로 만개의 레시피에 검색하고, 레시피 제목과 url 추출
+  - 목록 클릭 시 레시피 링크 자동 접속 구현
+    
+# 최종 결과
+![서비스_구동_녹화_영상_중](https://user-images.githubusercontent.com/108312150/191643230-01dd880a-c911-4956-a356-474b8c8cbf0c.gif)
+  
---
+++ pyqt_recipes.py
@@ -0,0 +1,110 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+import sys
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+# from lotte_final import *
+
+# 커널 충돌로 lotte.ipynb에서 만든 리스트가 불려오지 않아 임의로 리스트 정의함
+list_d = [{'key': '우유', 'sub_key': [('생크림', 0.7950142025947571), ('무염', 0.761357307434082), ('노른자', 0.7127785682678223), ('연유', 0.702159583568573), ('이스트', 0.6995631456375122), 
+            ('버터', 0.6953340768814087), ('파우더', 0.6717946529388428), ('베이킹파우더', 0.6663267612457275), ('빵가루', 0.6544387340545654), ('흰자', 0.6529161334037781)]}, 
+            {'key': '양지', 'sub_key': [('사태', 0.8905509114265442), ('샤브샤브', 0.8868820071220398), ('쇠고기', 0.8584051132202148), ('자른', 0.8546854257583618), ('미역국', 0.8422269225120544), 
+            ('갈비살', 0.8276513814926147), ('염장', 0.8213863372802734), ('토란', 0.8185833692550659), ('용소', 0.8079002499580383), ('방', 0.7932040095329285)]}, 
+            {'key': '토종닭', 'sub_key': [('라자냐', 0.9339334964752197), ('꼬리', 0.933007538318634), ('강정', 0.932475745677948), ('점', 0.9290907979011536), ('사진', 0.9290493726730347), 
+            ('삶기', 0.9243596792221069), ('민물', 0.9243115782737732), ('광어', 0.9209685325622559), ('랍스터', 0.9207404851913452), ('찰옥수수', 0.9197703003883362)]}, 
+            {'key': '갈비', 'sub_key': [('등뼈', 0.8643282055854797), ('등', 0.853766143321991), ('근', 0.808203935623169), ('돼지', 0.8056599497795105), ('돼지갈비', 0.7501478791236877), 
+            ('콜라', 0.7415348887443542), ('사태', 0.731940507888794), ('돈', 0.7301970720291138), ('찜', 0.7254461050033569), ('수육', 0.7155761122703552)]}]
+
+list_A = []
+
+for A in range(len(list_d)):
+    ori_A = list_d[A]['key']
+    ori_B = list_d[A]['sub_key']
+
+    list_A.append(ori_A)
+
+
+recipes_ui = 'C:/localRepository/recipes_project/recipes.ui'
+
+
+class MyWindow(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self, None)
+        uic.loadUi(recipes_ui, self)
+
+        self.pushButton.clicked.connect(self.pushButtonClicked)
+        # self.lineEdit.textChanged.connect(self.line_changed)
+        self.tblResult.itemSelectionChanged.connect(self.tblResultSelected)
+
+        self.comboBox.addItems(list_A)
+        self.comboBox.activated[str].connect(lambda :self.selectedComboItem(self.comboBox))
+        self.pushButton.clicked.connect(self.selectedComboItem)
+
+        self.show()
+
+ 
+    def line_changed(self):
+        # self.label_end.setText('')
+        pass
+ 
+    def pushButtonClicked(self):
+
+        url = f'https://www.10000recipe.com/recipe/list.html?q='
+        keyword = self.comboBox.currentText()
+        url_b = '&query=&order=date'
+        
+        result = requests.get(url + keyword + url_b)
+        bs_obj = BeautifulSoup(result.content, 'html.parser')
+        
+
+        self.tblResult.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tblResult.setColumnCount(2)
+        self.tblResult.setRowCount(10) 
+        self.tblResult.setHorizontalHeaderLabels(['레시피 이름', '레시피 링크'])
+        self.tblResult.setColumnWidth(0,350)
+        self.tblResult.setColumnWidth(1,250)
+        self.tblResult.setEditTriggers(QAbstractItemView.NoEditTriggers) # read only
+
+        ul = bs_obj.find('ul', class_='common_sp_list_ul ea4')
+        lis = ul.find_all('li', class_='common_sp_list_li')
+
+        list_title = []
+        list_url = []
+
+        for li in lis:
+            title = li.find('div',class_= 'common_sp_caption_tit line2').get_text()
+            list_title.append(title)
+
+            url = li.find('div', class_='common_sp_thumb')
+            url = url.find('a')['href']
+            url = f'https://www.10000recipe.com/{url}'
+            list_url.append(url)
+  
+
+        for i in range(len(list_title)):
+            self.tblResult.setItem(i,0,QTableWidgetItem(list_title[i]))
+            self.tblResult.setItem(i,1,QTableWidgetItem(list_url[i]))
+
+    def tblResultSelected(self) -> None:
+        selected = self.tblResult.currentRow() # 현재 선택된 열의 인덱스
+        link = self.tblResult.item(selected, 1).text()
+        webbrowser.open(link)
+
+
+    ### 콤보박스 관련 함수
+    def selectedComboItem(self,text):
+        # print(self.comboBox.currentText())
+        pass
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    Dialog = MyWindow()
+    Dialog.show()
+    app.exec_()
